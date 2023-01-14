db43d6bf7fc11f0ddf40e47ffaf23cfa6a244271
Ronny <ronny12301@gmail.com>
2023-01-14 12:49:56 -0600

    slider bar fills the progress

--- src/controllers/MediaPlayerFXMLController.java
+++ src/controllers/MediaPlayerFXMLController.java
@@ -1,14 +1,43 @@
 package controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
+
 /**
  * FXML Controller class
  *
  * @author ronny12301
  */
-public class MediaPlayerFXMLController  {
+public class MediaPlayerFXMLController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */ 
+    @FXML
+    Slider progressSlider;
     
+    @FXML
+    ProgressBar progressBar;
+    
+    @FXML
+    Slider volumeSlider;
+
+    @FXML
+    ProgressBar volumeBar;
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        visualProgression(progressSlider, progressBar);
+        visualProgression(volumeSlider, volumeBar);
+    }
+    
+    
+    
+    private void visualProgression(Slider slider, ProgressBar bar) {
+        slider.valueProperty().addListener((obs, oldValue, newValue) -> {
+            bar.setProgress(newValue.doubleValue()/slider.getMax());
+        });
+    }
 }
