49fff7627afcf793a28175d3cac47eca417c958a
Ronny <ronny12301@gmail.com>
2023-01-15 22:40:52 -0600

    Media reproduction implemented

--- src/controllers/MediaPlayerFXMLController.java
+++ src/controllers/MediaPlayerFXMLController.java
@@ -1,11 +1,19 @@
 package controllers;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.DoubleProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.FileChooser;
 
 /**
  * FXML Controller class
@@ -15,23 +23,54 @@ import javafx.scene.control.Slider;
 public class MediaPlayerFXMLController implements Initializable {
 
     @FXML
-    Slider progressSlider;
+    private Slider progressSlider;
     
     @FXML
-    ProgressBar progressBar;
+    private ProgressBar progressBar;
     
     @FXML
-    Slider volumeSlider;
+    private Slider volumeSlider;
 
     @FXML
-    ProgressBar volumeBar;
+    private ProgressBar volumeBar;
+    
+    @FXML
+    private MediaView mediaView;
+    
+    @FXML
+    private VBox screenBackground;
 
+    private String path;
+    private MediaPlayer mediaPlayer;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         visualProgression(progressSlider, progressBar);
         visualProgression(volumeSlider, volumeBar);
     }
+    
+    
+    public void chooseFileClicked(ActionEvent e) {
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(null);
+        path = file.toURI().toString();
+        
+        if (path != null) {
+            Media media = new Media(path);
+            mediaPlayer = new MediaPlayer(media);
+            mediaView.setMediaPlayer(mediaPlayer);
+
+            mediaView.fitHeightProperty().bind(screenBackground.widthProperty());
+            mediaView.fitWidthProperty().bind(screenBackground.heightProperty());
+      
+            mediaPlayer.play();
+        }
+    }
+    
+    
+    
+    
+    
     
     
     
